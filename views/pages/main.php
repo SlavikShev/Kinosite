5a289ee6e55782d10b2bf6e6ee88662f1f586ad4
Someone <rgbjkbs@hdbf.ru>
2020-11-27 19:38:57 +0200

    was changed style.css, and main.php

--- views/pages/main.php
+++ views/pages/main.php
@@ -1,20 +1,17 @@
 <div class="main-box_content">
-<h2>Welcome to the greatest film site</h2>
-<?php if (count($this->films)): ?>
-    <?php foreach ($this->films as $film): ?>
-        <div class="content-table">
-            <h2>
-                <td><?= $film['name'] ?></td>
-            </h2>
-            <pre>
-                <td><?= $film['description'] ?></td>
+    <h2>Welcome to the greatest film site</h2>
+    <?php if (count($this->films)): ?>
+        <?php foreach ($this->films as $film): ?>
+            <div class="content-table">
+                <h2>
+                    <?= $film['name'] ?><
+                </h2>
+                <pre><?= $film['description'] ?>
             </pre>
-            <p>
-                <td><?= $film['year'] ?></td>
-            </p>
-        </div>
-    <?php endforeach; ?>
-<?php else: ?>
-<p>Sorry, there are no films yet</p>
-<?php endif ?>
+                <p>Year:<?= $film['year'] ?></p>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>Sorry, there are no films yet</p>
+    <?php endif ?>
 </div>
