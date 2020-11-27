0e506104dcf46e687a929e3753ce39694d5ed9af
rpushko <maraffonskiy@gmail.com>
2020-11-27 19:03:23 +0200

    added news showing

--- views/pages/main.php
+++ views/pages/main.php
@@ -1,8 +1,18 @@
-<h2>Films</h2>
-<?php foreach ($this->films as $film): ?>
-    <table>
-        <tr>
-            <td><?= $film['title'] ?></td>
-        </tr>
-    </table>
-<?php endforeach; ?>
+<h2>Welcome to the greatest film site</h2>
+<?php if (count($this->films)): ?>
+    <?php foreach ($this->films as $film): ?>
+        <table>
+            <tr>
+                <td><?= $film['name'] ?></td>
+            </tr>
+            <tr>
+                <td><?= $film['description'] ?></td>
+            </tr>
+            <tr>
+                <td><?= $film['year'] ?></td>
+            </tr>
+        </table>
+    <?php endforeach; ?>
+<?php else: ?>
+<p>Sorry, there are no films yet</p>
+<?php endif ?>
