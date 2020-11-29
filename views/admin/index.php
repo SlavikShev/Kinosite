7607a9d7df1cf91d4c7600c798214204c3b029b3
kate <ketrina113@gmail.com>
2020-11-29 22:20:18 +0200

    changed view of index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,71 +1,26 @@
-<div>
-<table>
-    <caption>Films</caption>
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>year</th>
-        <th>name</th>
-    </tr>
-    </thead>
-    <?php if (count($this->films)): ?>
-        <tbody>
-        <?php foreach ($this->films as $film): ?>
-            <tr>
-                <td><?= $film['id'] ?></td>
-                <td><?= $film['year'] ?></td>
-                <td><?= $film['name'] ?></td>
-                <td>
-                    <form method="post" action="/admin/delete">
-                        <input type="hidden" name="id" value="<?= $film['id'] ?>">
-                        <button type="submit">Delete</button>
-                    </form>
-                </td>
-                <td>
-                    <form method="post" action="/admin/edit">
-                        <input type="hidden" name="id" value="<?= $film['id'] ?>">
-                        <button type="submit">Edit</button>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    <?php endif; ?>
-</table>
-<a href="/admin/create">Create</a>
-</div>
-<div>
-    <table>
-        <caption>Users</caption>
-        <thead>
-        <tr>
-            <th>#ID</th>
-            <th>Login</th>
-            <th>Action</th>
-        </tr>
-        </thead>
-        <?php if(count($this->users)): ?>
-            <tbody>
-                <?php foreach($this->users as $user): ?>
-                    <tr>
-                        <td><?= $user['id'] ?></td>
-                        <td><?= $user['login'] ?></td>
-                        <?php if($user['id'] > 1) :?>
-                        <td>
-                            <form method="post" action="/admin/deleteuser">
-                                <input type="hidden" name="userid" value="<?= $user['id'] ?>">
-                                <button type="submit">Delete</button>
-                            </form>
-                        </td>
-                        <?php else :?>
-                        <td>
-                            I'm the GOD :D
-                        </td>
-                        <?php endif;?>
-                    </tr>
-                <?php endforeach;?>
-            </tbody>
-        <?php endif; ?>
-    </table>
-    <a href="/admin/createaccount">Create</a>
+<div class="usersBtns">
+    <a href="/admin/users">
+        <div class="usersMenu">
+            <i class="fas fa-user-friends"></i>
+            Users
+        </div>
+    </a>
+    <a href="/admin/films">
+        <div class="usersMenu">
+            <i class="fas fa-film"></i>
+            Films
+        </div>
+    </a>
+    <a href="/admin/createaccount">
+        <div class="usersMenu">
+            <i class="fas fa-user-plus"></i>
+            Add User
+        </div>
+    </a>
+    <a href="/admin/create">
+        <div class="usersMenu">
+            <i class="far fa-file-video"></i>
+            Add Film
+        </div>
+    </a>
 </div>
