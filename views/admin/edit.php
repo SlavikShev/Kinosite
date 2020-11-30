9571e2c23f0b92be392a40806b4f605300034e2d
SlavikShev <12061998sl>
2020-11-30 14:52:42 +0300

    admin panel redesign

--- views/admin/edit.php
+++ views/admin/edit.php
@@ -1,7 +1,12 @@
-<form method="post" action="/admin/update">
-    <input type="hidden" name="id" value="<?= $this->film['id'] ?>">
-    <label>Year: <input type="year" name="year" value="<?= $this->film['year'] ?>" required></label>
-    <label>Name: <input type="text" name="name" value="<?= $this->film['name'] ?>" required></label>
-    <label>Description: <textarea name="description"><?= $this->film['description'] ?></textarea></label>
-    <input type="submit" value="Send">
+<form method="post" action="/admin/update" class="decor">
+    <div class="form-left-decoration"></div>
+    <div class="form-right-decoration"></div>
+    <div class="circle"></div>
+    <div class="form-inner">
+        <input type="hidden" name="id" value="<?= $this->film['id'] ?>">
+        <label>Year: <input type="year" name="year" value="<?= $this->film['year'] ?>" required></label>
+        <label>Name: <input type="text" name="name" value="<?= $this->film['name'] ?>" required></label>
+        <label>Description: <textarea name="description"><?= $this->film['description'] ?></textarea></label>
+        <input type="submit" value="Send">
+    </div>
 </form>
